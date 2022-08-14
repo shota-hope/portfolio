1842b87b48b7cd69e6f6d0d7a8ac03ab974f493f
shota-hope <ddeeppaa.gogo@gmail.com>
2022-08-14 18:03:38 +0900

    mantaineとスクリーンサイズを統一

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {},
   },
   plugins: [],
