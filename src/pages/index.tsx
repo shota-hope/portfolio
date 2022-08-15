03bdc399ba0cce221914c1d8bc7ef18165ac0755
shota-hope <ddeeppaa.gogo@gmail.com>
2022-08-15 10:48:45 +0900

    feat: フッター作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Footer />
     </div>
   );
 };
